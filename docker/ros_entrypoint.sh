e809944ca2c0341f322d9bf2344e21cc6b7e9e4c
Filipopo <4060824+filipopo@users.noreply.github.com>
2026-07-09 14:27:56 +0200

    Fix permissions in docker

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
-source "/root/ros2_ws/install/setup.bash" --
+source "/opt/ros2_ws/install/setup.bash" --
 
 exec "$@"
