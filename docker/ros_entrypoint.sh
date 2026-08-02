0704ead04e6440f5c09f2dbc24609c9172ffb06c
Filip <4060824+filipopo@users.noreply.github.com>
2026-08-02 22:26:12 +0200

    set ROS_DISTRO in the container entrypoint if it's missing
    Add logic to set ROS_DISTRO if it's not defined.

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+# set ROS_DISTRO if it's missing
+if [ -z "$ROS_DISTRO" ]; then
+  for dir in /opt/ros/*; do
+    if [ -f "$dir/setup.bash" ]; then
+      export ROS_DISTRO=$(basename "$dir")
+    fi
+  done
+fi
+
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
 source "/opt/ros2_ws/install/setup.bash" --
